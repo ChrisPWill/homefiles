c7cf82789e1e830f2e7894b2fc4d73ee31b70b51
Chris Williams <cwilliams@atlassian.com>
2023-04-12 12:13:34 +1000

    More zsh aliases

--- nix/home/home-manager/programs/zsh.nix
+++ nix/home/home-manager/programs/zsh.nix
@@ -2,6 +2,20 @@
   enable = true;
   shellAliases = {
     home-update = "home-manager switch";
+
+    # Disable autocorrection for these
+    ln = "nocorrect ln";
+    mv = "nocorrect mv";
+    mkdir = "nocorrect mkdir";
+    sudo = "nocorrect sudo";
+
+    # Directory navigation
+    ".." = "cd ..";
+    "..." = "cd ../..";
+    "...." = "cd ../../..";
+    "-- -" = "cd -";
+    "-- --" = "cd -2";
+    "-- ---" = "cd -3";
   };
 
   # History options
@@ -17,5 +31,5 @@
     expireDuplicatesFirst = true;
   };
 
-  
+
 };
