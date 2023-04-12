821db6fcca962a9f265a0bde212e6b6e5ba0c322
Chris Williams <chris@chrispwill.com>
2023-04-12 11:18:43 +1000

    Fix create-links script

--- create-links.sh
+++ create-links.sh
@@ -11,9 +11,3 @@ function link_and_backup_destination {
 
 link_and_backup_destination "nix/etc/nix/nix.conf" "/etc/nix/nix.conf"
 link_and_backup_destination "nix/home/home-manager" "$HOME/.config/home-manager"
-
-echo Choose a machine config
-ls "${SCRIPT_DIR}/nix/home/home-manager/machines"
-read -r machine_name
-ln -sf "${SCRIPT_DIR}/nix/home/home-manager/machines/${machine_name}" "${SCRIPT_DIR}/nix/home/home-manager/current-machine.nix"
-ln -sf "${SCRIPT_DIR}/nix/home/home-manager/machines/${machine_name}" "$HOME/.config/home-manager/current-machine.nix"
