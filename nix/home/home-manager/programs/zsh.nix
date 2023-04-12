5a3823c2b1c435270431ba0d18579e01e59e94b4
Chris Williams <cwilliams@atlassian.com>
2023-04-12 14:13:52 +1000

    Add missing vim config

--- nix/home/home-manager/programs/zsh.nix
+++ nix/home/home-manager/programs/zsh.nix
@@ -103,6 +103,153 @@ bindkey "^I" expand-or-complete-with-dots
 
   initExtra = ''
 # }}}
+# zle widgets {{{
+# -----------------------------------------------------------------------------
+# The ZLE widges are all followed by "zle -<MODE> <NAME>" and bound below in the "Key Bindings" section.
+
+# Delete all characters between a pair of characters. Mimics Vim's "di" text
+# object functionality.
+delete-in() {
+    # Create locally-scoped variables we'll need
+    local CHAR LCHAR RCHAR LSEARCH RSEARCH COUNT
+    # Read the character to indicate which text object we're deleting.
+    read -k CHAR
+    if [ "$CHAR" = "w" ]
+        then # diw, delete the word.
+        # find the beginning of the word under the cursor
+        zle vi-backward-word
+        # set the left side of the delete region at this point
+        LSEARCH=$CURSOR
+        # find the end of the word under the cursor
+        zle vi-forward-word
+        # set the right side of the delete region at this point
+        RSEARCH=$CURSOR
+        # Set the BUFFER to everything except the word we are removing.
+        RBUFFER="$BUFFER[$RSEARCH+1,''\${#BUFFER}]"
+        LBUFFER="$LBUFFER[1,$LSEARCH]"
+        return
+    # diw was unique. For everything else, we just have to define the
+    # characters to the left and right of the cursor to be removed
+    elif [ "$CHAR" = "(" ] || [ "$CHAR" = ")" ] || [ "$CHAR" = "b" ]
+    then # di), delete inside of a pair of parenthesis
+        LCHAR="("
+        RCHAR=")"
+    elif [ "$CHAR" = "[" ] || [ "$CHAR" = "]" ]
+    then # di], delete inside of a pair of square brackets
+        LCHAR="["
+        RCHAR="]"
+    elif [ $CHAR = "{" ] || [ $CHAR = "}" ] || [ "$CHAR" = "B" ]
+    then # di], delete inside of a pair of braces
+        LCHAR="{"
+        RCHAR="}"
+    else
+        # The character entered does not have a special definition.
+        # Simply find the first instance to the left and right of the
+        # cursor.
+        LCHAR="$CHAR"
+        RCHAR="$CHAR"
+    fi
+    # Find the first instance of LCHAR to the left of the cursor and the
+    # first instance of RCHAR to the right of the cursor, and remove
+    # everything in between.
+    # Begin the search for the left-sided character directly the left of the cursor.
+    LSEARCH=''\${#LBUFFER}
+    # Keep going left until we find the character or hit the beginning of the buffer.
+    while [ "$LSEARCH" -gt 0 ] && [ "$LBUFFER[$LSEARCH]" != "$LCHAR" ]
+    do
+        LSEARCH=$(expr $LSEARCH - 1)
+    done
+    # If we hit the beginning of the command line without finding the character, abort.
+    if [ "$LBUFFER[$LSEARCH]" != "$LCHAR" ]
+    then
+        return
+    fi
+    # start the search directly to the right of the cursor
+    RSEARCH=0
+    # Keep going right until we find the character or hit the end of the buffer.
+    while [ "$RSEARCH" -lt $(expr ''\${#RBUFFER} + 1 ) ] && [ "$RBUFFER[$RSEARCH]" != "$RCHAR" ]
+    do
+        RSEARCH=$(expr $RSEARCH + 1)
+    done
+    # If we hit the end of the command line without finding the character, abort.
+    if [ "$RBUFFER[$RSEARCH]" != "$RCHAR" ]
+    then
+        return
+fi
+# Set the BUFFER to everything except the text we are removing.
+    RBUFFER="$RBUFFER[$RSEARCH,''\${#RBUFFER}]"
+    LBUFFER="$LBUFFER[1,$LSEARCH]"
+}
+zle -N delete-in
+
+
+# Delete all characters between a pair of characters and then go to insert mode.
+# Mimics Vim's "ci" text object functionality.
+change-in() {
+    zle delete-in
+    zle vi-insert
+}
+zle -N change-in
+
+# Delete all characters between a pair of characters as well as the surrounding
+# characters themselves. Mimics Vim's "da" text object functionality.
+delete-around() {
+    zle delete-in
+    zle vi-backward-char
+    zle vi-delete-char
+    zle vi-delete-char
+}
+zle -N delete-around
+
+# Delete all characters between a pair of characters as well as the surrounding
+# characters themselves and then go into insert mode Mimics Vim's "ca" text
+# object functionality.
+change-around() {
+    zle delete-in
+    zle vi-backward-char
+    zle vi-delete-char
+    zle vi-delete-char
+    zle vi-insert
+}
+zle -N change-around
+
+# Zsh's vi-up/down-line-or-history does what I want but leaves the cursor at the
+# beginning rather than front. Perplexing!
+vim-up-line-or-history() {
+    zle vi-up-line-or-history
+    zle vi-end-of-line
+}
+zle -N vim-up-line-or-history
+vim-down-line-or-history() {
+    zle vi-down-line-or-history
+    zle vi-end-of-line
+}
+zle -N vim-down-line-or-history
+
+# The hackneyed <Nop> bind.
+nop() {}
+zle -N nop
+
+# Use clipboard rather than system registers.
+prepend-x-selection () {
+    RBUFFER=$(xsel -op </dev/null)$RBUFFER;
+    zle vi-end-of-line
+}
+zle -N prepend-x-selection
+append-x-selection () {
+    zle vi-forward-char
+    RBUFFER=$(xsel -op </dev/null)$RBUFFER;
+    zle vi-end-of-line
+}
+zle -N append-x-selection
+yank-x-selection () {
+    print -rn -- $CUTBUFFER | xsel -ip;
+}
+zle -N yank-x-selection
+autoload edit-command-line
+zle -N edit-command-line
+
+# }}}
 # The Vim setup {{{
 # -----------------------------------------------------------------------------
 
