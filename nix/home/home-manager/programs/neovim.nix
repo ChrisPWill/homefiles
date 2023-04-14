ab4f9aa9f9b64eb8a28e2b478906b5d190490316
Chris Williams <chris@chrispwill.com>
2023-04-14 15:02:07 +1000

    NvimTree keybinds

--- nix/home/home-manager/programs/neovim.nix
+++ nix/home/home-manager/programs/neovim.nix
@@ -170,6 +170,11 @@ in {
           group_empty = true,
         },
       })
+      vim.keymap.set('n', '<leader>ntt', '<cmd>NvimTreeFindFile<cr><cmd>NvimTreeFocus<cr>', { noremap = true })
+      vim.keymap.set('n', '<leader>ntf', '<cmd>NvimTreeFindFile<cr>', { noremap = true })
+      vim.keymap.set('n', '<leader>ntr', '<cmd>NvimTreeRefresh<cr>', { noremap = true })
+      vim.keymap.set('n', '<leader>nt[', '<cmd>NvimTreeCollapse<cr>', { noremap = true })
+      vim.keymap.set('n', '<leader>ntx', '<cmd>NvimTreeClose<cr>', { noremap = true })
 
       -- enabled languages for later config
       local enabledLanguages = vim.json.decode('${builtins.toJSON enabledLanguages}')
