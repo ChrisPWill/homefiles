9b22d27629b4137ba80da6e9e6a5f330dcb83077
Chris Williams <chris@chrispwill.com>
2023-04-14 13:24:52 +1000

    Get back my x

--- nix/home/home-manager/programs/neovim.nix
+++ nix/home/home-manager/programs/neovim.nix
@@ -101,6 +101,11 @@ in {
 
       -- Movement plugins
       require('leap').add_default_mappings()
+      vim.keymap.del({'x', 'o'}, 'x')
+      vim.keymap.del({'x', 'o'}, 'X')
+      -- To set alternative keys for "exclusive" selection:
+      vim.keymap.set({'x', 'o'}, '<M-x>', '<Plug>(leap-forward-till)')
+      vim.keymap.set({'x', 'o'}, '<M-X>', '<Plug>(leap-backward-till)')
       require('flit').setup{}
 
       -- Mini plugins
