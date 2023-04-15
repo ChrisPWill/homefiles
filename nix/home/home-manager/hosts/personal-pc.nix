5cf6ba47fc22cff7b34bae141b39c0b0064dfafb
Chris Williams <chris@chrispwill.com>
2023-04-15 14:37:43 +1000

    Add typescript to personal pc

--- nix/home/home-manager/hosts/personal-pc.nix
+++ nix/home/home-manager/hosts/personal-pc.nix
@@ -1,7 +1,14 @@
 {pkgs}: {
   userEmail = "chris@chrispwill.com";
   extraModules = [];
+  enabledLanguages = [
+    "javascript"
+    "typescript"
+    "html"
+  ];
   extraPackages = with pkgs; [
     nodePackages.prettier
+    nodePackages.typescript
+    nodePackages.typescript-language-server
   ];
 }
