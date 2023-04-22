fd8d4f4632276c39a95e8e834091a3128c670047
Chris Williams <chris@chrispwill.com>
2023-04-22 15:38:26 +1000

    Fix position of lsp setup

--- nix/home/home-manager/programs/neovim.nix
+++ nix/home/home-manager/programs/neovim.nix
@@ -8,7 +8,6 @@
       (builtins.readFile ./neovim/bufferline-config.lua)
       (builtins.readFile ./neovim/telescope-config.lua)
       (builtins.readFile ./neovim/formatter-config.lua)
-      "lsp.setup()" # Call LSP setup at the end
     ]
     ++ (
       if builtins.elem "typescript" enabledLanguages
@@ -33,8 +32,10 @@
     ++ (
       if builtins.elem "dockerfile" enabledLanguages
       then [(builtins.readFile ./neovim/dockerfile-language-server-config.lua)]
-      else []
-    );
+      else [])
+        ++
+      (["lsp.setup()"]); # Call LSP setup at the end
+
   languageToTreesitterName = language:
     {
       # Add language name mappings here if treesitter uses a different name
