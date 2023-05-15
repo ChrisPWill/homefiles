63c975fe22b37f303268dd15eeaddf2779ef6d81
Chris Williams <chris@chrispwill.com>
2023-05-15 15:45:59 +1000

    Fix input format

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "ChrisPWill Nix flake";
 
   inputs = {
-    nixpkgs.url = "github.nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github.nix-community/home-manager";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
