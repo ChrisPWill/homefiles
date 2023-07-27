ba57c4716e2911ba1028a494543b65b71e965b0b
Chris Williams <chris@chrispwill.com>
2023-07-27 15:16:02 +1000

    Disable wlr

--- systems/x86_64-linux/hyprland/default.nix
+++ systems/x86_64-linux/hyprland/default.nix
@@ -33,7 +33,7 @@ in {
   services.dbus.enable = true;
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
+    # wlr.enable = true;
     extraPortals = [pkgs.xdg-desktop-portal-gtk];
   };
 
