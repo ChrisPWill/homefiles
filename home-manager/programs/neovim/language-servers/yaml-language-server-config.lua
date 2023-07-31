3dc3a822d5eb643ff242487ebfa9102c652fbdb4
Chris Williams <chris@chrispwill.com>
2023-07-31 11:13:08 +1000

    Fix yaml schemas

--- home-manager/programs/neovim/language-servers/yaml-language-server-config.lua
+++ home-manager/programs/neovim/language-servers/yaml-language-server-config.lua
@@ -1,3 +1,13 @@
 lspconfig.yamlls.setup({
   on_attach=on_attach,
+  settings= {
+    yaml = {
+      schemaStore = {
+        enable = true,
+      },
+      schemas = {
+        ['https://micros.prod.atl-paas.net/schema.json'] = "/**/(*.sd.y*ml|service-descriptor.y*ml)",
+      },
+    },
+  }
 })
