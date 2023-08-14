843cff9bf8d96d48609dc64b864e11c135cdb1b6
Chris Williams <chris@chrispwill.com>
2023-08-14 16:17:40 +1000

    Include home-manager in system packages

--- systems/aarch64-darwin/default.nix
+++ systems/aarch64-darwin/default.nix
@@ -3,9 +3,12 @@ let
 in {
   name = "aarch64darwin";
   system = darwinArmSystem;
-  extraModules = {lib, ...}: [
+  extraModules = {pkgs, lib, ...}: [
     {
       nixpkgs.hostPlatform = lib.mkDefault darwinArmSystem;
+      environment.systemPackages = with pkgs; [
+        home-manager
+      ];
     }
   ];
 }
