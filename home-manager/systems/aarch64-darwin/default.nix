c17691a061604a4752b3c6bedf68ac04c6c36ef9
Chris Williams <chris@chrispwill.com>
2023-08-14 16:21:13 +1000

    Add aarch64-darwin to home-manager

---
+++ home-manager/systems/aarch64-darwin/default.nix
@@ -0,0 +1,20 @@
+{
+  pkgs-unstable,
+  lib,
+  theme,
+  programsPath,
+  ...
+}: {
+  homeDirPrefix = "/Users";
+  extraPrograms = {
+    alacritty = import (programsPath + "/alacritty.nix") {
+      pkgs = pkgs-unstable;
+      transparency = 0.90;
+      inherit theme;
+      systemIsDarwin = true;
+    };
+    vscode = import (programsPath + "/vscode.nix") {inherit theme;};
+  };
+  extraPackages = [];
+  extraModules = [];
+}
